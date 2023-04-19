592d3552dff4e8d850f0699ce8841dac462e69a2
Saba Okriashvili <sabaaoqrish@gmail.com>
2023-04-19 08:06:11 +0400

    finished JS

--- script.js
+++ script.js
@@ -4,24 +4,17 @@ loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let triviaAmount = document.getElementById("triviaAmount");
-  let triviaCategory = document.getElementById("username");
+  let triviaCategory = document.getElementById("triviaCategory");
   let triviaDifficulty = document.getElementById("triviaDifficulty");
-  let triviaType = document.getElementById("username");
+  let triviaType = document.getElementById("triviaType");
 
   if (triviaAmount.value == "" ) {
     alert("Ensure you input a value in all fields!");
   } else {
     alert("This form has been successfully submitted!");
     console.log(
-      `This form has a Number of Questions: ${triviaAmount.value}, Category of: ${triviaCategory.value} and Difficulty is set to ${triviaDifficulty.value}. Type of answers will be ${triviaType.value}.`
+      `This form has a Number of Questions: ${triviaAmount.value}, Category of: ${triviaCategory.value} and Difficulty is set to ${triviaDifficulty.value}.Type of answers will be ${triviaType.value}.`
     );
-
-    triviaAmount.value = "";
-    triviaCategory.value = "";
-    triviaDifficulty.value = "";
-    triviaType.value = "";
-
   }
 });
 
-console.log(triviaCategory.value)
