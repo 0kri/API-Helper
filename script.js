5e44115971c3a8ca391ddb31dbd2470f6920c7c6
Saba Okriashvili <sabaaoqrish@gmail.com>
2023-04-20 12:25:52 +0400

    add url

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let loginForm = document.getElementById("loginForm");
 
+let apiData = {
+  url: "https://opentdb.com/api.php?",
+  amount: 1,
+  category: "any",
+  diffuculty: "any",
+  type: "any"
+}
+
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -15,6 +23,25 @@ loginForm.addEventListener("submit", (e) => {
     console.log(
       `This form has a Number of Questions: ${triviaAmount.value}, Category of: ${triviaCategory.value} and Difficulty is set to ${triviaDifficulty.value}.Type of answers will be ${triviaType.value}.`
     );
+
+    fetchData().then( response => console.log(response))
   }
 });
+
+async function fetchData() {
+  // const { url, amount, category, diffuculty, type } = apiData
+  
+  apiData.amount = triviaAmount.value
+  apiData.category = triviaCategory.value
+  apiData.diffuculty = triviaDifficulty.value
+  apiData.type = triviaType.value
+
+  const { url, amount, category, diffuculty, type } = apiData
+
+  let quizUrl = `${url}amount=${amount}&category=${category}&difficulty=${diffuculty}&type=${type}`
+  const response = await fetch(quizUrl)
+  return response.json()
+  console.log(quizUrl)
+
+}
 
